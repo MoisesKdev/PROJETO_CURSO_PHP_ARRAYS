6d1521bba597b38dad80ca4122a500a03eafa6ff
MoisesKdev <moisesk.dev@outlook.com>
2022-04-21 10:15:26 -0300

    Acrescentando visibilidade ao projeto PHP com HTML

--- banco.php
+++ banco.php
@@ -16,13 +16,26 @@ $contasCorrentes = [
         'saldo' => 100
     ]
 ];
-titularComLetrasMaiusculas ($contasCorrentes['124.356.790-80']);
 
+titularComLetrasMaiusculas ($contasCorrentes['148-259-367-00']);
+?>
 
-$contasCorrentes['123.456.789-10'] = sacar($contasCorrentes['123.456.789-10'], 100);
-$contasCorrentes['124.356.790-80'] = depositar($contasCorrentes['124.356.790-80'],-90);
+<!-- Inicio do HTML -->
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Contas Correntes</h1>
 
-foreach ($contasCorrentes as $cpf => $conta) {
-    ['titular' => $titular, 'saldo' => $saldo] = $conta;
-    exibeMensagem("$cpf $titular $saldo");
-}
+    <dl>
+        <?php foreach ($contasCorrentes as $cpf => $conta) {?>
+        <dt><h2><?= $conta['titular'];?> - <?=$cpf;?></h2></dt>
+        <dd>Saldo: <?= $conta['saldo']?> R$</dd>
+        <?php } ?>
+    </dl>
+</body>
+</html>
